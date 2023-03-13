e3ee2f20939b4e3fdf8d52432f9def8a2fd596f0
Taein Min <maintain0404@gmail.com>
2023-03-13 20:05:18 +0900

    :sparkles: Add healthcheck REST endpoint

---
+++ src/test/java/com/taein/springboot/example/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.taein.springboot.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    
+    @RequestMapping("/health")
+    public String health() {
+        return "Healthy";
+    }
+}
