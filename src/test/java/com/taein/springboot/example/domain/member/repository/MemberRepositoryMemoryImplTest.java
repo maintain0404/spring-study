452421c8a9a90716683c853a4d48f65a063dec82
Taein Min <maintain0404@gmail.com>
2023-03-14 16:36:43 +0900

    :white_check_mark: Add test for MemberRepositoryMemoryImpl.

---
+++ src/test/java/com/taein/springboot/example/domain/member/repository/MemberRepositoryMemoryImplTest.java
@@ -0,0 +1,8 @@
+package com.taein.springboot.example.domain.member.repository;
+
+public class MemberRepositoryMemoryImplTest implements MemberRepositoryTest<MemberRepositoryMemoryImpl>{
+ 
+    public MemberRepositoryMemoryImpl createMemberRepository() {
+        return new MemberRepositoryMemoryImpl();
+    }
+}
